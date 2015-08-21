7c80f66ceeb9ef2e7182eab0a23a26fa1ade8b40
Wei Song <wsong83@gmail.com>
2015-08-21 10:12:12 +0100

    still commit the endian coverter for potential future use

---
+++ common/script/endian_convert.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+open (my $file, '<', $ARGV[0]) or die 'cannot read input mem file';
+
+while(<$file>) {
+    #print;
+    chomp;
+    @n = unpack("C*", $_);
+    for(my $i=0; $i<@n/8; $i++) {
+        print pack("C*", 
+            $n[$i*8+6], $n[$i*8+7],
+            $n[$i*8+4], $n[$i*8+5],
+            $n[$i*8+2], $n[$i*8+3],
+            $n[$i*8+0], $n[$i*8+1]);
+    }
+    print "\n"
+}
+
