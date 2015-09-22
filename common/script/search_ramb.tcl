cbc198f54e74de89441db739fcb99897630707b9
Wei Song <wsong83@gmail.com>
2015-09-22 17:22:22 +0100

    improve ramb search script and bump boot ram

--- common/script/search_ramb.tcl
+++ common/script/search_ramb.tcl
@@ -9,7 +9,11 @@ open_project $orig_proj_dir/$project_name.xpr
 open_run impl_1
 
 # search for all RAMB blocks
-report_property  [get_cells ram_reg_0] {LOC}
-report_property  [get_cells ram_reg_1] {LOC}
-report_property  [get_cells ram_reg_2] {LOC}
-report_property  [get_cells ram_reg_3] {LOC}
+foreach m [get_cells ram_reg_*] { put $m; report_property $m {LOC} }
+#report_property  [get_cells ram_reg_1] {LOC}
+#report_property  [get_cells ram_reg_2] {LOC}
+#report_property  [get_cells ram_reg_3] {LOC}
+#report_property  [get_cells ram_reg_4] {LOC}
+#report_property  [get_cells ram_reg_5] {LOC}
+#report_property  [get_cells ram_reg_6] {LOC}
+#report_property  [get_cells ram_reg_7] {LOC}
