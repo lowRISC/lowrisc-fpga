d23f6253b59cf710c5a9a2468fc7b6eca2f40488
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2017-04-06 13:28:14 +0100

    Remove kernel from QSPI memory download

--- common/script/cfgmem.tcl
+++ common/script/cfgmem.tcl
@@ -3,12 +3,10 @@
 # Function:
 #   Download bitstream to FPGA
 
-set boot [lindex $argv 2]
 set bit [lindex $argv 1]
 set device [lindex $argv 0]
 
-puts "BOOT: $boot"
 puts "BITSTREAM: $bit"
 puts "DEVICE: $device"
 
-write_cfgmem -force -format mcs -interface spix4 -size 128 -loadbit "up 0x0 $bit" -loaddata "up 0x00400000 $boot" -file "$bit.mcs"
+write_cfgmem -force -format mcs -interface spix4 -size 128 -loadbit "up 0x0 $bit" -file "$bit.mcs"
