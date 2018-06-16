6df8e79d7ed9375afd5293169e7f1cb169d0edfd
Dr Jonathan Kimmitt <jrrk2@cam.ac.uk>
2018-06-16 14:36:33 +0100

    Update memory map

--- bare_metal/driver/lowrisc_memory_map.h
+++ bare_metal/driver/lowrisc_memory_map.h
@@ -14,13 +14,15 @@ extern volatile uint64_t * get_ddr_base();
 extern volatile uint64_t   get_ddr_size();
 extern volatile uint64_t * get_flash_base();
 
-enum {  bram_base_addr = 0x40000000,
-        sd_base_addr   = 0x40010000,        
-        sd_bram_addr   = 0x40018000,
-        eth_base_addr  = 0x40020000,
+enum { clint_base_addr = 0x02000000,
+	plic_base_addr = 0x0c000000,
+	bram_base_addr = 0x40000000,
+          sd_base_addr = 0x40010000,        
+          sd_bram_addr = 0x40018000,
+         eth_base_addr = 0x40020000,
         keyb_base_addr = 0x40030000, // These have been relocated
         uart_base_addr = 0x40034000,
-        vga_base_addr  = 0x40038000,
+         vga_base_addr = 0x40038000,
       };
-      
+
 #endif
