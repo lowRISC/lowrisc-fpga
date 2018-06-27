6d63161ba408cd789557ece065f5be61d785c6ce
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-06-27 20:04:29 +0100

    Add config.h for mmc boot loader

---
+++ bare_metal/examples/include/config.h
@@ -0,0 +1,8 @@
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/LowRISC/ariane
+#include <config_defaults.h>
+#include <config_uncmd_spl.h>
+#include <configs/lowrisc-ariane.h>
+#include <asm/config.h>
+#include <linux/kconfig.h>
+#include <config_fallbacks.h>
