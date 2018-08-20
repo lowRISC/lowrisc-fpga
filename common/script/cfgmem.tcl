449a581c686266081261b95f21df36d779c940ae
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-08-20 12:33:12 +0100

    Update .gitignore in kc705, force cfgmem overwrite

--- common/script/cfgmem.tcl
+++ common/script/cfgmem.tcl
@@ -9,4 +9,4 @@ set device [lindex $argv 0]
 puts "BITSTREAM: $bit"
 puts "DEVICE: $device"
 
-write_cfgmem -format MCS -size 128 -interface BPIx16 -loadbit "up 0x0 $bit" "$bit.mcs"
+write_cfgmem -force -format MCS -size 128 -interface BPIx16 -loadbit "up 0x0 $bit" "$bit.mcs"
